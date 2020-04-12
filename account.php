671b46b740e19d3fc3cd35d44451b235bb3778b7
Lindsay Park <lindsaypark@Lindsays-MacBook-Air-2.local>
2020-04-11 21:32:05 -0400

    deleted extra connect-db

--- account.php
+++ account.php
@@ -1,5 +1,5 @@
 <?php
-require('connect-db.php');
+require('connectdb.php');
 require('form_handling.php');
 ?>
 <?php
--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-require('connect-db.php');
+require('connectdb.php');
 // require('account.php');
 ?>
 
